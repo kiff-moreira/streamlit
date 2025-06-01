f8b540a707511aafe5ded18b2fe37a264c7eef6b
kiff-moreira <kiff.moreira.dev@gmail.com>
2025-06-01 13:31:35 -0300

    Add files via upload

---
+++ curriculo.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+
+
+col1, col2 = st.columns([2,1])
+with col1:
+    st.title(' Kiff Fernandes')
+
+with col2:
+    st.image('./kiff.jpg')
+
+
+'''
+- 31-996405896
+- kiff.fernades@gmail.com
+
+## Sobre mim.
+
+desenvolver full-stack recem formado
+
+ 
+## Habilidades
+- Ruby on rails
+- Python
+- C++
+
+## Experiencias
+
+Empresario, técnico em fibra óptica( fusao, instalação e manutenção)
+
+## Fomarção academica
+Ensino Superior (Rede de computadores - PROMOVE)
+
+
+'''
